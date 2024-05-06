e979e5072928cef9c1805c1843bf95e6b4aae5ff
nasiiim <44586155+nasiiim@users.noreply.github.com>
2024-05-06 13:31:46 +0200

    Time: 72 ms (88.72%), Space: 55.8 MB (99.11%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+let n = nums.length;
+    k = k % n;
+
+    function reverse(start, end) {
+        while (start < end) {
+            let temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+}
+    reverse(0, n - 1);
+    reverse(0, k - 1);
+    reverse(k, n - 1);
+}
