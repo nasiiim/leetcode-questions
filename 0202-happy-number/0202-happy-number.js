9097a8d1cd3b3888cb7db1e1d00a3c4cb6ce47b5
nasiiim <44586155+nasiiim@users.noreply.github.com>
2024-06-05 11:26:37 +0200

    Time: 44 ms (97.91%), Space: 49.3 MB (86.43%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let sum = 0;
+  while (n > 0) {
+    let e = n % 10;
+    n = Math.floor(n / 10);
+    sum += e * e;
+  }
+  if (sum === 1) {
+    return true;
+  } else if (sum > 1 && sum <= 4) {
+    return false;
+  }
+  return isHappy(sum)
+};
