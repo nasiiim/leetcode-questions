dedbf72abfed1f1a04d56ac89315c3dcda29e91d
nasiiim <44586155+nasiiim@users.noreply.github.com>
2024-08-05 23:54:10 +0200

    Time: 49 ms (89.05%), Space: 49.9 MB (55.9%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  
+    let left = 0;
+    let right = numbers.length-1
+    while(left < right) {
+        const cur = numbers[left] + numbers[right];
+       
+        if(cur === target) return [left+1, right+1]
+   
+        cur > target ? right-- : left++
+    }
+};
