53a7b4717539f2209f5277ee035392e6896a1de6
nasiiim <44586155+nasiiim@users.noreply.github.com>
2024-10-01 23:19:23 +0200

    Time: 60 ms (29.71%), Space: 49.5 MB (46.05%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0;
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i+1] - prices[i] > 0){
+            profit += prices[i+1] - prices[i];
+        }
+    }
+    
+    return profit;
+};
